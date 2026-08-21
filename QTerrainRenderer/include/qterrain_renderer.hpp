eb3c8bee843e32a61a74ef4ead36c5979e895ef0
Otto Link <otto.link.bv@gmail.com>
2026-08-21 20:25:29 +0200

    build: modernize CMake with target-based config and fix logging rules macro

--- QTerrainRenderer/include/qterrain_renderer.hpp
+++ QTerrainRenderer/include/qterrain_renderer.hpp
@@ -2,6 +2,11 @@
    License. The full license is in the file LICENSE, distributed with this software. */
 #pragma once
 
+#ifndef QTR_QPUTENV_QT_LOGGING_RULES
+#define QTR_QPUTENV_QT_LOGGING_RULES                                                     \
+  "qt.widgets.gestures.debug=false;qt.qpa.*=false;qt.pointer.*=false"
+#endif
+
 #include "qtr/camera.hpp"
 #include "qtr/config.hpp"
 #include "qtr/imgui_widgets.hpp"
